ae10953c7434ef4ce95809ce7592e9484beb7b0a
Vinicius Agostini <vinicius.agostini19@gmail.com>
2024-02-21 19:00:13 +0100

    use pgpool to implement adding a subscription

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use std::net::TcpListener;
 
+use sqlx::{Connection, PgPool};
 use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let db_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
 
-    run(listener)?.await
+    run(listener, db_pool)?.await
 }
